efb1a50ee96a2a8bf1c18ae85860fefaaa7e2db7
spalger <spalger@users.noreply.github.com>
2016-01-14 12:58:24 -0700

    added esjs

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,17 @@
+/// <reference path='../ts-definitions/elasticsearch.d.ts' />
+
 import {Component} from 'angular2/core';
+import elasticsearch from 'elasticsearch';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
+export class AppComponent {
 
-export class AppComponent { }
+  private _client: elasticsearch.ClientInterface
+
+  constructor() {
+    this._client = elasticsearch.Client();
+  }
+}
